7796d1249bc8a0f657ff429b0b8977362fe2f32f
Junjun <junjunruan@gmail.com>
2017-09-11 22:49:49 -0400

    add cornerstone lib and window leveling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+    templateUrl: './app.component.html',
 })
-export class AppComponent  { name = 'Angular'; }
+export class AppComponent  {
+  title = 'app';
+  @ViewChild('canvas') private _canvas: ElementRef;
+  private _element: any = undefined;
+  private _windowWidth = 256;
+  private _windowCenter = 128;
+
+  constructor(public _elementRef: ElementRef) {}
+
+  ngAfterViewInit(): void {
+    const imageId = 'https://i.imgur.com/u8VVJSO.jpg';
+    this._element = this._elementRef.nativeElement.parentNode;
+    // Enable the element with Cornerstone
+    cornerstone.enable(this._element, this._canvas.nativeElement);
+    // Load the image and enable tools
+    cornerstone.loadImage(imageId).then((image: Object, athis: Object) => {
+      // by default, it will display still to fit to window
+      cornerstone.displayImage(this._element, image);
+    });
+  }
+
+  /**
+   * Method to do window leveling.
+   */
+  onClick(): void {
+    this._windowWidth += 10;
+    this._windowCenter += 10;
+    if (this._element !== undefined) {
+      if (cornerstone.getViewport(this._element) !== undefined) {
+        let vwport: any = cornerstone.getViewport(this._element);
+        vwport.voi.windowWidth = this._windowWidth;
+        vwport.voi.windowCenter = this._windowWidth;
+        cornerstone.setViewport(this._element, vwport);
+      }
+    }
+  }
+}
